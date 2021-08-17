271d42cd8326eb087feee574e454e1684c85d234
fe-admin <13488881765@163.com>
2021-08-17 22:59:30 +0800

    表单添加验证错误跳转逻辑

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,38 +7,46 @@
 import debounces from "lodash.debounce";
 
 export function sleep(time) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve();
-        }, time);
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
 }
 
 export function debounce(fn, time = 1000, options = { leading: true }) {
-    return debounces(fn, time, options);
+  return debounces(fn, time, options);
 }
 
 export const toUpperCase = ([a, ...b]) => a.toUpperCase() + `${b}`;
 
 export function random(length = -6) {
-    return Math.random()
-        .toString(36)
-        .slice(length);
+  return Math.random()
+    .toString(36)
+    .slice(length);
 }
 
 export function addRoutes(router, list) {
-    if (Array.isArray(list) && router) {
-        list.forEach((item) => {
-            router.addRoute(item);
-        });
-    }
+  if (Array.isArray(list) && router) {
+    list.forEach((item) => {
+      router.addRoute(item);
+    });
+  }
 }
 
 export function getDicMap(list) {
-    const map = {};
-    list.forEach((item) => {
-      map[item.value] = item.label;
-    });
-    return map;
-  }
-  
+  const map = {};
+  list.forEach((item) => {
+    map[item.value] = item.label;
+  });
+  return map;
+}
+
+export function scrollIntoView(ele) {
+  console.info(ele);
+  const target = ele || document.querySelector(".is-error");
+  target?.scrollIntoView({
+    block: "center",
+    behavior: "smooth",
+  });
+}
--- src/views/form/FormBase/index.vue
+++ src/views/form/FormBase/index.vue
@@ -1,10 +1,14 @@
 <template src="./tpl.html"> </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Ref, Vue } from "vue-property-decorator";
+import { ElForm } from "element-ui/types/form";
+import { sleep, scrollIntoView } from "@/utils";
 
 @Component({})
 export default class FormBase extends Vue {
+  @Ref("form") readonly formEle!: ElForm;
+  @Ref("title") readonly titleEle!: ElForm;
   loading = false;
   editeRowIndex = -1;
   editeRow = {};
@@ -13,12 +17,66 @@ export default class FormBase extends Vue {
     date: "",
     desc: "",
     radio: 0,
-    weight: NaN,
+    weight: 0,
     standard: "",
     invitation: "",
     user: "",
     area: null,
   };
+  formRule = {
+    title: [
+      {
+        required: true,
+        message: "请输入标题",
+        trigger: ["change", "blur"],
+      },
+    ],
+    date: [
+      {
+        required: true,
+        message: "请选择起止日期",
+        trigger: ["change", "blur"],
+      },
+    ],
+
+    desc: [
+      {
+        required: true,
+        message: "请输入目标描述",
+        trigger: ["change", "blur"],
+      },
+    ],
+    standard: [
+      {
+        required: true,
+        message: "请输入衡量标准",
+        trigger: ["change", "blur"],
+      },
+    ],
+  };
+  resetForm(): void {
+    this.formEle.resetFields();
+    scrollIntoView(this.titleEle.$el);
+  }
+  submitForm(): void {
+    this.formEle.validate(async (res) => {
+      if (res) {
+        this.loading = true;
+        await sleep(1000);
+        this.$notify({
+          title: "成功",
+          message: "添加成功！",
+          type: "success",
+        });
+        await sleep(1000);
+        this.resetForm();
+      } else {
+        await this.$nextTick();
+        scrollIntoView();
+      }
+      this.loading = false;
+    });
+  }
 }
 </script>
 <style lang="scss" src="./style.scss" scoped></style>
